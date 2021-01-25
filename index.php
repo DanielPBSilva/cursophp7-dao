ba80438c0473023c3fa06399cbe8710515d3fb6e
Daniel Pinheiro <danielpbsilva10@gmail.com>
2021-01-25 16:00:05 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
+
+/* Chama apenas um usuário
 $root = new Usuario();
-
-$root->loadById(1);
-
+$root->loadById(4);
 echo $root;
+*/ 
 
+// carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// carrega usuarios buscando pelo login
+//$search = Usuario::search("daniel");
+//echo json_encode($search);
+
+// carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Daniel","123456");
+
+echo $usuario;
 ?>
